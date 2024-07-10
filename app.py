cf6930bc7f0b98756f6ab1931132fa84e2aa0768
TANMAY GANGURDE <tanmaygangurde20@gmail.com>
2024-07-10 20:23:56 +0530

    vercel changes

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ def predict():
     message = {"answer": response}
     return jsonify(message)
 
-if __name__ == "__main__":  # Correct the typo here
-    app.run(debug=True)
+
     
  
---
+++ wsgi.py
@@ -0,0 +1,3 @@
+from app import app
+if __name__ == "__main__":  # Correct the typo here
+    app.run(debug=True)
