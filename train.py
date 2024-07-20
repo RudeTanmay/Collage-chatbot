676e8ab3f9b4642d17395fcc2910b50c20f13028
Tanmay Gangurde <143246915+RudeTanmay@users.noreply.github.com>
2024-07-20 23:41:50 +0530

    convert pytorch to tensorflow

--- train.py
+++ train.py
@@ -1,61 +1,124 @@
-#train
+#This is Train.py
+import numpy as np
 import random
 import json
+import nltk
+from nltk.corpus import wordnet, stopwords
+from nltk.stem import WordNetLemmatizer
+import string
 
-import torch
+import tensorflow as tf
 
-from model import NeuralNet
-from nltk_utils import bag_of_words, tokenize
+from model import create_model
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# Download necessary NLTK data
+nltk.download('punkt', quiet=True)
+nltk.download('wordnet', quiet=True)
+nltk.download('stopwords', quiet=True)
 
-with open('intents.json', 'r') as json_data:
-    intents = json.load(json_data)
+lemmatizer = WordNetLemmatizer()
+stop_words = set(stopwords.words('english'))
 
-FILE = "data.pth"
-data = torch.load(FILE)
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
 
-input_size = data["input_size"]
-hidden_size = data["hidden_size"]
-output_size = data["output_size"]
-all_words = data['all_words']
-tags = data['tags']
-model_state = data["model_state"]
+def lemmatize_word(word):
+    return lemmatizer.lemmatize(word.lower())
 
-model = NeuralNet(input_size, hidden_size, output_size).to(device)
-model.load_state_dict(model_state)
-model.eval()
+def remove_stopwords(words):
+    return [word for word in words if word not in stop_words and word not in string.punctuation]
 
-bot_name = "Sam"
+def bag_of_words(tokenized_sentence, words):
+    sentence_words = [lemmatize_word(word) for word in tokenized_sentence]
+    bag = np.zeros(len(words), dtype=np.float32)
+    for idx, w in enumerate(words):
+        if w in sentence_words: 
+            bag[idx] = 1
+    return bag
 
-def get_response(msg):
-    sentence = tokenize(msg)
-    X = bag_of_words(sentence, all_words)
-    X = X.reshape(1, X.shape[0])
-    X = torch.from_numpy(X).to(device)
+with open('intents.json', 'r') as f:
+    intents = json.load(f)
 
-    output = model(X)
-    _, predicted = torch.max(output, dim=1)
+all_words = []
+tags = []
+xy = []
 
-    tag = tags[predicted.item()]
+for intent in intents['intents']:
+    tag = intent['tag']
+    tags.append(tag)
+    for pattern in intent['patterns']:
+        w = tokenize(pattern)
+        all_words.extend(w)
+        xy.append((w, tag))
 
-    probs = torch.softmax(output, dim=1)
-    prob = probs[0][predicted.item()]
-    if prob.item() > 0.75:
-        for intent in intents['intents']:
-            if tag == intent["tag"]:
-                return random.choice(intent['responses'])
+all_words = [lemmatize_word(w) for w in all_words if w not in string.punctuation]
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
+
+print(len(xy), "patterns")
+print(len(tags), "tags:", tags)
+print(len(all_words), "unique lemmatized words:", all_words)
+
+X_train = []
+y_train = []
+for (pattern_sentence, tag) in xy:
+    bag = bag_of_words(pattern_sentence, all_words)
+    X_train.append(bag)
+    label = tags.index(tag)
+    y_train.append(label)
+
+X_train = np.array(X_train)
+y_train = np.array(y_train)
+
+num_epochs = 1000
+batch_size = 8
+learning_rate = 0.001
+input_size = len(X_train[0])
+hidden_size = 8
+output_size = len(tags)
+print(input_size, output_size)
+
+model = create_model(input_size, hidden_size, output_size)
+
+optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+@tf.function
+def train_step(words, labels):
+    with tf.GradientTape() as tape:
+        predictions = model(words, training=True)
+        loss = loss_object(labels, predictions)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    return loss
+
+for epoch in range(num_epochs):
+    idx = np.random.permutation(len(X_train))
+    X_train, y_train = X_train[idx], y_train[idx]
     
-    return "Please check our website for more information..."
+    for i in range(0, len(X_train), batch_size):
+        batch_X = X_train[i:i+batch_size]
+        batch_y = y_train[i:i+batch_size]
+        
+        loss = train_step(batch_X, batch_y)
+    
+    if (epoch+1) % 100 == 0:
+        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.numpy():.4f}')
 
+print(f'final loss: {loss.numpy():.4f}')
 
-if __name__ == "__main__":
-    print("Let's chat! (type 'quit' to exit)")
-    while True:
-        # sentence = "do you use credit cards?"
-        sentence = input("You: ")
-        if sentence == "quit":
-            break
+data = {
+    "model": model,
+    "input_size": input_size,
+    "hidden_size": hidden_size,
+    "output_size": output_size,
+    "all_words": all_words,
+    "tags": tags
+}
 
-        resp = get_response(sentence)
-        print(resp)
+FILE = "data.pkl"
+import pickle
+with open(FILE, 'wb') as f:
+    pickle.dump(data, f)
+
+print(f'training complete. file saved to {FILE}')
